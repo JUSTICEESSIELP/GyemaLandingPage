a428e0e0bb21baeea78ab82f505169f6287f54c7
JUSTICEESSIELP <justicessiel@gmail.com>
2024-09-15 21:29:08 +0000

    changed

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -183,7 +183,7 @@ export function LandingPageComponent() {
                     alt="Sam Amponsah"
                   />
                   <div className="mt-4">
-                    <h3 className="text-lg font-medium text-gray-900">Sam Amponsah</h3>
+                    <h3 className="text-lg font-medium text-gray-900">Samuel Osei-Amponsah.</h3>
                     <p className="text-sm text-gray-500">Knowledge Expert in Climate</p>
                   </div>
                 </div>
